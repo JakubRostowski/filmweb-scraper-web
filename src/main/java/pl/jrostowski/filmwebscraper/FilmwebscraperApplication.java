63d0079c7951ded066d392cf4c2813d713ec0d50
JakubRostowski <rostowskijakub@gmail.com>
2021-10-01 10:15:48 +0200

    initial commit

---
+++ src/main/java/pl/jrostowski/filmwebscraper/FilmwebscraperApplication.java
@@ -0,0 +1,13 @@
+package pl.jrostowski.filmwebscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmwebscraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmwebscraperApplication.class, args);
+    }
+
+}
