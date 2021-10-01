bc06adbca0b19a33be9f321ccbc971dcef464b59
JakubRostowski <rostowskijakub@gmail.com>
2021-10-01 10:44:49 +0200

    archived movie entity created

--- src/main/java/pl/jrostowski/filmwebscraper/entity/ArchivedMovie.java
+++ src/main/java/pl/jrostowski/filmwebscraper/entity/ArchivedMovie.java
@@ -1,2 +1,42 @@
-package pl.jrostowski.filmwebscraper.entity;public class ArchivedMovie {
+package pl.jrostowski.filmwebscraper.entity;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class ArchivedMovie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long archivedMovieId;
+
+    @ManyToOne
+    @JoinColumn(name="movieId")
+    private Movie movieId;
+
+    private int position;
+    private String title;
+    private double rate;
+    private double criticsRate;
+    private Timestamp timeOfCreation;
+    private Timestamp timeOfModification = new Timestamp(System.currentTimeMillis());
+
+    @Override
+    public String toString() {
+        return "ArchivedMovie{" +
+                "archivedMovieId=" + archivedMovieId +
+                ", movieId=" + movieId +
+                ", position=" + position +
+                ", title='" + title + '\'' +
+                ", rate=" + rate +
+                ", criticsRate=" + criticsRate +
+                ", timeOfCreation=" + timeOfCreation +
+                ", timeOfModification=" + timeOfModification +
+                '}';
+    }
 }
