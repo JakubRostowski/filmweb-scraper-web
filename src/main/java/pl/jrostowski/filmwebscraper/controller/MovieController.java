fe39579ff1b42bda862b29497ffdc49e545d7f03
JakubRostowski <rostowskijakub@gmail.com>
2021-10-01 12:47:05 +0200

    prototype of controller created

--- src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
@@ -1,2 +1,24 @@
-package pl.jrostowski.filmwebscraper.controller;public class MovieController {
+package pl.jrostowski.filmwebscraper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.jrostowski.filmwebscraper.entity.Movie;
+import pl.jrostowski.filmwebscraper.service.MovieService;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+public class MovieController {
+
+    private final MovieService service;
+
+    public MovieController(MovieService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/display")
+    public Map<Integer, Movie> displayFilmwebData() throws IOException {
+        return service.downloadData();
+    }
 }
