9b7adfa5ab2f49d262825070fe4264eb5266f173
JakubRostowski <rostowskijakub@gmail.com>
2021-10-12 14:58:38 +0200

    controller created

--- src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
@@ -1,2 +1,24 @@
-package pl.jrostowski.filmwebscraper.controller;public class MovieController {
+package pl.jrostowski.filmwebscraper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.jrostowski.filmwebscraper.entity.Movie;
+import pl.jrostowski.filmwebscraper.service.MovieService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService service;
+
+    @GetMapping("/movies")
+    public String showDatabaseMovies(Model model) {
+        List<Movie> movies = service.getMovieContent();
+        model.addAttribute("movies", movies);
+        return "moviesList";
+    }
 }
