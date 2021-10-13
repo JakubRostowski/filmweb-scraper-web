21a01c81f632da4b66f9bdbea11514a2e141b4e9
JakubRostowski <rostowskijakub@gmail.com>
2021-10-13 10:50:18 +0200

    user controller created

--- src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
@@ -1,2 +1,25 @@
-package pl.jrostowski.filmwebscraper.controller;public class UserController {
+package pl.jrostowski.filmwebscraper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.jrostowski.filmwebscraper.entity.User;
+import pl.jrostowski.filmwebscraper.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public String showDatabaseMovies(Model model) {
+        List<User> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "usersList";
+    }
+
 }
