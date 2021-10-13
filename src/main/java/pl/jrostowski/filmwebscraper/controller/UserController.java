597c04749b21ed74695b31a361c90c7de20808f0
JakubRostowski <rostowskijakub@gmail.com>
2021-10-13 15:13:37 +0200

    adding users implemented

--- src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.jrostowski.filmwebscraper.entity.User;
 import pl.jrostowski.filmwebscraper.service.UserService;
 
@@ -20,6 +22,19 @@ public class UserController {
         List<User> users = userService.findAllUsers();
         model.addAttribute("users", users);
         return "usersList";
+    }
+
+    @GetMapping("/users/add")
+    public String getAddUserForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "userAddForm";
+    }
+
+    @PostMapping("users/save")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users/";
     }
 
 }
