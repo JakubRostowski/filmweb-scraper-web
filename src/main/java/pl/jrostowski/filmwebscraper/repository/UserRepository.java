bde6370d3f843809f28cd64633ec51f1a011e285
JakubRostowski <rostowskijakub@gmail.com>
2021-10-13 10:48:51 +0200

    user repository created

--- src/main/java/pl/jrostowski/filmwebscraper/repository/UserRepository.java
+++ src/main/java/pl/jrostowski/filmwebscraper/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package pl.jrostowski.filmwebscraper.repository;public interface UserRepository {
+package pl.jrostowski.filmwebscraper.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.jrostowski.filmwebscraper.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 }
