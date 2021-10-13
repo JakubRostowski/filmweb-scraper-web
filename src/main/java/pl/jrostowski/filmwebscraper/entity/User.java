e0d2fbdd110c04776a9cdc58edc5a18efdedb12b
JakubRostowski <rostowskijakub@gmail.com>
2021-10-13 10:48:12 +0200

    user entity created

--- src/main/java/pl/jrostowski/filmwebscraper/entity/User.java
+++ src/main/java/pl/jrostowski/filmwebscraper/entity/User.java
@@ -1,2 +1,26 @@
-package pl.jrostowski.filmwebscraper.entity;public class User {
+package pl.jrostowski.filmwebscraper.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long userId;
+
+    @NonNull private String name;
+    @NonNull private String email;
+    @NonNull private String password;
+    private Timestamp timeOfCreation = new Timestamp(System.currentTimeMillis());
+    private Timestamp timeOfModification = new Timestamp(System.currentTimeMillis());
+
 }
