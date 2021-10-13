e02329871f037bed04e6133b0bf09726848c6c2b
JakubRostowski <rostowskijakub@gmail.com>
2021-10-13 10:49:35 +0200

    user service created

--- src/main/java/pl/jrostowski/filmwebscraper/service/UserService.java
+++ src/main/java/pl/jrostowski/filmwebscraper/service/UserService.java
@@ -1,2 +1,23 @@
-package pl.jrostowski.filmwebscraper.service;public class UserService {
+package pl.jrostowski.filmwebscraper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.jrostowski.filmwebscraper.entity.User;
+import pl.jrostowski.filmwebscraper.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> findAllUsers() {
+        List<User> result = new ArrayList<>();
+        userRepository.findAll().forEach(result::add);
+        return result;
+    }
+
 }
