019c726737a3fa5c8b91864bfb68b29b3cbdd6ef
JakubRostowski <rostowskijakub@gmail.com>
2021-10-21 20:25:37 +0200

    security fields added

--- src/main/java/pl/jrostowski/filmwebscraper/entity/User.java
+++ src/main/java/pl/jrostowski/filmwebscraper/entity/User.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@Table(name = "users")
 @RequiredArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -17,9 +18,11 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long userId;
 
-    @NonNull private String name;
+    @NonNull private String username;
     @NonNull private String email;
     @NonNull private String password;
+    @NonNull private int enabled = 1;
+    @NonNull private String role = "ROLE_USER";
     private Timestamp timeOfCreation = new Timestamp(System.currentTimeMillis());
     private Timestamp timeOfModification = new Timestamp(System.currentTimeMillis());
 
