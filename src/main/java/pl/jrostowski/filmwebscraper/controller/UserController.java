8ffbc6044de25d365a8c615d58931ecdb894b9f7
JakubRostowski <rostowskijakub@gmail.com>
2021-10-21 20:26:26 +0200

    password encoding added

--- src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
@@ -1,6 +1,7 @@
 package pl.jrostowski.filmwebscraper.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,9 @@ public class UserController {
 
     @PostMapping("users/save")
     public String saveUser(@ModelAttribute("user") User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userService.save(user);
         return "redirect:/users/";
     }
