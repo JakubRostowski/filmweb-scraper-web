d95d151498a8907be492c2a05a3a869a333a13ab
JakubRostowski <rostowskijakub@gmail.com>
2021-10-25 19:09:48 +0200

    PasswordMatch annotation created

--- src/main/java/pl/jrostowski/filmwebscraper/validation/PasswordMatch.java
+++ src/main/java/pl/jrostowski/filmwebscraper/validation/PasswordMatch.java
@@ -1,2 +1,26 @@
-package pl.jrostowski.filmwebscraper.validation;public @interface PasswordMatch {
+package pl.jrostowski.filmwebscraper.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = PasswordMatchValidator.class)
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PasswordMatch {
+    String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    String password();
+    String confirmPassword();
+
+    @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List
+    {
+        PasswordMatch[] value();
+    }
 }
--- src/main/java/pl/jrostowski/filmwebscraper/validation/PasswordMatchValidator.java
+++ src/main/java/pl/jrostowski/filmwebscraper/validation/PasswordMatchValidator.java
@@ -1,2 +1,39 @@
-package pl.jrostowski.filmwebscraper.validation;public class PasswordMatchValidator {
+package pl.jrostowski.filmwebscraper.validation;
+
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch, Object> {
+    private String password;
+    private String confirmPassword;
+    private String message;
+
+    @Override
+    public void initialize(final PasswordMatch constraintAnnotation) {
+        password = constraintAnnotation.password();
+        confirmPassword = constraintAnnotation.confirmPassword();
+        message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        boolean valid = true;
+        try {
+            final Object firstObj = new BeanWrapperImpl(value).getPropertyValue(password);
+            final Object secondObj = new BeanWrapperImpl(value).getPropertyValue(confirmPassword);
+
+            valid = firstObj == null && secondObj == null || firstObj != null && firstObj.equals(secondObj);
+        } catch (final Exception ignore) {}
+
+        if (!valid) {
+            context.buildConstraintViolationWithTemplate(message)
+                    .addPropertyNode(password)
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+        }
+
+        return valid;
+    }
 }
