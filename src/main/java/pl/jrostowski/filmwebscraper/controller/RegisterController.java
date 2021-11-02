e3b906ad37556f60ef28af3f9760bc8e3a141e25
JakubRostowski <rostowskijakub@gmail.com>
2021-11-02 13:38:51 +0100

    user controller splitted for clarity

--- src/main/java/pl/jrostowski/filmwebscraper/controller/RegisterController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/RegisterController.java
@@ -1,2 +1,51 @@
-package pl.jrostowski.filmwebscraper.controller;public class RegisterController {
+package pl.jrostowski.filmwebscraper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.jrostowski.filmwebscraper.entity.User;
+import pl.jrostowski.filmwebscraper.forms.UserForm;
+import pl.jrostowski.filmwebscraper.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class RegisterController {
+
+    private final UserService userService;
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+        UserForm userForm = new UserForm();
+        model.addAttribute("user", userForm);
+        return "register";
+    }
+
+    @PostMapping("register/save")
+    public String saveUser(@Valid @ModelAttribute("user") UserForm userForm, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "register";
+        }
+        if (userService.findByUsername(userForm.getUsername()) != null) {
+            model.addAttribute("registrationError", "Username already exists.");
+            return "register";
+        }
+        if (userService.findByEmail(userForm.getEmail()) != null) {
+            model.addAttribute("registrationError", "Email already exists.");
+            return "register";
+        }
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(userForm.getPassword());
+        userForm.setPassword(encodedPassword);
+        User user = new User(userForm.getUsername(), userForm.getEmail(), userForm.getPassword());
+        userService.save(user);
+        return "redirect:/users/";
+    }
 }
--- src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/UserController.java
@@ -1,18 +1,12 @@
 package pl.jrostowski.filmwebscraper.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import pl.jrostowski.filmwebscraper.entity.User;
-import pl.jrostowski.filmwebscraper.forms.UserForm;
 import pl.jrostowski.filmwebscraper.service.UserService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -26,40 +20,6 @@ public class UserController {
         List<User> users = userService.findAllUsers();
         model.addAttribute("users", users);
         return "users-list";
-    }
-
-    @GetMapping("/users/add")
-    public String getRegisterForm(Model model) {
-        UserForm userForm = new UserForm();
-        model.addAttribute("user", userForm);
-        return "register";
-    }
-
-    @PostMapping("users/save")
-    public String saveUser(@Valid @ModelAttribute("user") UserForm userForm, Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            return "register";
-        }
-        if (userService.findByUsername(userForm.getUsername()) != null) {
-            model.addAttribute("registrationError", "Username already exists.");
-            return "register";
-        }
-        if (userService.findByEmail(userForm.getEmail()) != null) {
-            model.addAttribute("registrationError", "Email already exists.");
-            return "register";
-        }
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(userForm.getPassword());
-        userForm.setPassword(encodedPassword);
-        User user = new User(userForm.getUsername(), userForm.getEmail(), userForm.getPassword());
-        userService.save(user);
-        return "redirect:/users/";
-    }
-
-    @GetMapping("/login")
-    public String showMyLoginPage() {
-        return "login";
     }
 
 }
