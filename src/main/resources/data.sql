fb7647e86e9a61e74bcb2492428734b87718f0f6
JakubRostowski <rostowskijakub@gmail.com>
2022-04-09 20:14:18 +0200

    auto increment user id fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO users(id, email, enabled, password, role, time_of_creation, time_of_
 VALUES (1, 'user@example.com', 1, '$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua', 'ROLE_USER',
         '2022-03-17 20:17:39.537000', '2022-03-17 20:17:39.537000', 'user')
 ON CONFLICT DO NOTHING;
-
+SELECT setval(pg_get_serial_sequence('users', 'id'), max(id)) FROM users;
