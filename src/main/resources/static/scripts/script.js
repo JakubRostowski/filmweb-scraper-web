5fddfb43a7f3e5fd5f4a123e5c2668d97d5cfcee
JakubRostowski <rostowskijakub@gmail.com>
2022-04-15 21:22:16 +0200

    animation applied to tiles

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector(".menu");
 const menuOpenBtn = document.querySelector(".header .menu-icon");
+const tiles = document.querySelectorAll(".tiles div");
 
 menuOpenBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     menu.classList.toggle("active");
     menuOpenBtn.classList.toggle("active");
+    tiles.forEach(tile => tile.classList.toggle("active"));
 }
