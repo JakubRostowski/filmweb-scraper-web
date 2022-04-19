45429d21b1d495a1f85f179fa85a271b13847cb4
JakubRostowski <rostowskijakub@gmail.com>
2022-04-19 17:37:43 +0200

    hotfix for data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,14 @@
 DO
-$$
+'
+    DECLARE
     BEGIN
-        IF NOT EXISTS(SELECT COUNT(*)
-                      FROM users) THEN
+        IF NOT EXISTS(SELECT *
+                      FROM users
+                      WHERE id = 1) THEN
             INSERT INTO users (email, enabled, password, role, time_of_creation, time_of_modification, username)
-            VALUES ('user@example.com', true, '$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua',
-                    'ROLE_ADMIN',
-                    '2022-03-17 20:17:39.537000', '2022-03-17 20:17:39.537000', 'user');
+            VALUES (''user@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
+                    ''ROLE_ADMIN'',
+                    ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''user'');
         END IF;
     END;
-$$ LANGUAGE PLPGSQL;
+' LANGUAGE PLPGSQL;
