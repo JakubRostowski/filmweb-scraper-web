41369d156c2036c48b5d41caf9d5cd099121dbdc
JakubRostowski <rostowskijakub@gmail.com>
2022-04-27 00:13:41 +0200

    page-menu modified

---
+++ src/main/resources/static/scripts/page-menu.js
@@ -0,0 +1,16 @@
+const input = document.querySelector(".page-menu input");
+const maxPage = document.querySelector(".page-menu .page-max").textContent;
+
+input.addEventListener('keyup', redirectIfCorrect);
+
+function redirectIfCorrect(e) {
+    if (input.value) {
+        if (input.value < 1) {
+            input.value = 1;
+        } else if (input.value > maxPage) {
+            input.value = parseInt(maxPage);
+        } else if (e.key === 'Enter') {
+            location.href = 'http://localhost:8080/admin-panel/page/' + input.value;
+        }
+    }
+}
