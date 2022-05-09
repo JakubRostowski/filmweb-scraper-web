fe08f9c5f66cc62a28de7da2a5b080df8c5c288b
JakubRostowski <rostowskijakub@gmail.com>
2022-05-09 23:24:03 +0200

    charts for ratings added

---
+++ src/main/resources/static/scripts/chart.js
@@ -0,0 +1,36 @@
+function generateChart(valueSelector, chartSelector) {
+    const rate = parseInt(document.querySelector(valueSelector).textContent);
+    const data = {
+        datasets: [{
+            backgroundColor: ['rgb(25, 255, 25)', 'rgba(25, 255, 25, 0)'],
+            borderColor: ['black', 'black'],
+            borderWidth: [1, 0],
+            data: [rate, 10 - rate],
+        }]
+    };
+
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: false,
+            events: [],
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    };
+
+    return new Chart(
+        document.querySelector(chartSelector),
+        config
+    );
+}
+
+const rateChart = generateChart('.rate-value', '.chart-rate.rate');
+const criticsRateChart = generateChart('.critics-rate-value', '.chart-rate.critics-rate');
+
+
+
