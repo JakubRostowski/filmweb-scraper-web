c0a4d16c7dfae86596a3b61710fa96d057d60530
JakubRostowski <rostowskijakub@gmail.com>
2022-05-12 17:06:24 +0200

    redundant import removed

--- src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
+++ src/main/java/pl/jrostowski/filmwebscraper/controller/MovieController.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 import pl.jrostowski.filmwebscraper.entity.Movie;
 import pl.jrostowski.filmwebscraper.service.MovieService;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
