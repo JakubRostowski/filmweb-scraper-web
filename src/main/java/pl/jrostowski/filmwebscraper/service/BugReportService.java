d53d08ddcd835a64641e37d45ab4d0f47f326b8b
JakubRostowski <rostowskijakub@gmail.com>
2022-05-26 13:53:54 +0200

    redundant import removed

--- src/main/java/pl/jrostowski/filmwebscraper/service/BugReportService.java
+++ src/main/java/pl/jrostowski/filmwebscraper/service/BugReportService.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.jrostowski.filmwebscraper.entity.BugReport;
-import pl.jrostowski.filmwebscraper.entity.Post;
 import pl.jrostowski.filmwebscraper.repository.BugReportRepository;
 
 import java.util.Optional;
