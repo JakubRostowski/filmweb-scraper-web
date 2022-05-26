8d3bac0d7d477aedee6b4462149a7e2ef2811f19
JakubRostowski <rostowskijakub@gmail.com>
2022-05-27 00:46:46 +0200

    user added to data initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
 DO
 '
-    BEGIN
-        IF NOT EXISTS(SELECT *
-                      FROM users
-                      WHERE id = 1) THEN
-            INSERT INTO users (email, enabled, password, role, time_of_creation, time_of_modification, username)
-            VALUES (''user@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
-                    ''ROLE_ADMIN'',
-                    ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''user'');
-        END IF;
-    END;
+BEGIN
+IF NOT EXISTS(SELECT *
+FROM users
+WHERE id = 1) THEN
+    INSERT INTO users (email, enabled, password, role, time_of_creation, time_of_modification, username)
+    VALUES (''admin@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
+        ''ROLE_ADMIN'', ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''admin''),
+        (''user@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
+            ''ROLE_USER'', ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''user'');
+    END IF;
+END;
 ' LANGUAGE PLPGSQL;
