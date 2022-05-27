a74f1db22ac7d14b6ed827c5d9ee1d570da42247
JakubRostowski <rostowskijakub@gmail.com>
2022-05-27 13:46:23 +0200

    imports modified

--- src/test/java/pl/jrostowski/filmwebscraper/repository/FilmwebRepositoryTest.java
+++ src/test/java/pl/jrostowski/filmwebscraper/repository/FilmwebRepositoryTest.java
@@ -1,6 +1,8 @@
 package pl.jrostowski.filmwebscraper.repository;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.jrostowski.filmwebscraper.entity.Movie;
