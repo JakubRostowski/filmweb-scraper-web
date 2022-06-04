b0c7f785cfab00fdccc46756a1d631bbfcbadfd5
JakubRostowski <rostowskijakub@gmail.com>
2022-06-04 04:11:34 +0200

    undo change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,8 @@ DO
 '
 BEGIN
 IF NOT EXISTS(SELECT *
-FROM users) THEN
+FROM users
+WHERE id = 1) THEN
     INSERT INTO users (email, enabled, password, role, time_of_creation, time_of_modification, username)
     VALUES (''admin@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
         ''ROLE_ADMIN'', ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''admin''),
