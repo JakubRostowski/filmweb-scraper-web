18548cfc235cfae43732e8cf1f942df47b73a69a
JakubRostowski <rostowskijakub@gmail.com>
2022-06-04 04:06:58 +0200

    script fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,7 @@ DO
 '
 BEGIN
 IF NOT EXISTS(SELECT *
-FROM users
-WHERE id = 1) THEN
+FROM users) THEN
     INSERT INTO users (email, enabled, password, role, time_of_creation, time_of_modification, username)
     VALUES (''admin@example.com'', true, ''$2a$10$UG80r83ao4PPsv8vgI3G1OHfjaAfCYZS3/29y76b6gRMW74qJiUua'',
         ''ROLE_ADMIN'', ''2022-03-17 20:17:39.537000'', ''2022-03-17 20:17:39.537000'', ''admin''),
