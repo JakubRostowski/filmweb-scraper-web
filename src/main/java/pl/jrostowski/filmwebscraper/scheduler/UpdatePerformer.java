e93581bb4ab5d3bb47f55b4d7245b1c627948c12
JakubRostowski <rostowskijakub@gmail.com>
2023-02-01 15:07:49 +0100

    imports optimized

--- src/main/java/pl/jrostowski/filmwebscraper/scheduler/UpdatePerformer.java
+++ src/main/java/pl/jrostowski/filmwebscraper/scheduler/UpdatePerformer.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import pl.jrostowski.filmwebscraper.service.MovieService;
 
 import java.io.IOException;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
