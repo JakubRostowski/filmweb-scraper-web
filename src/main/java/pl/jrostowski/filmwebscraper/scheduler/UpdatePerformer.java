2e34e8845cf1addf4236e8132aef04dac97e1e4c
JakubRostowski <rostowskijakub@gmail.com>
2023-02-01 13:47:44 +0100

    updating at startup implemented

--- src/main/java/pl/jrostowski/filmwebscraper/scheduler/UpdatePerformer.java
+++ src/main/java/pl/jrostowski/filmwebscraper/scheduler/UpdatePerformer.java
@@ -1,6 +1,9 @@
 package pl.jrostowski.filmwebscraper.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import pl.jrostowski.filmwebscraper.service.MovieService;
@@ -10,9 +13,19 @@ import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
-public class UpdateScheduler {
+public class UpdatePerformer {
+
+    @Value("${scraper.update-after-startup}")
+    private boolean updateAfterStartup;
 
     private final MovieService movieService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void downloadMoviesAfterStartup() throws IOException {
+        if (updateAfterStartup) {
+            updateDatabase();
+        }
+    }
 
     @Scheduled(cron = "${cron.update-value}")
     public void updateDatabase() throws IOException {
