4f7c33fbd85abe0e2f4f3c24ce2ee6ba35f7acb6
JakubRostowski <rostowskijakub@gmail.com>
2023-02-15 04:10:03 +0100

    set exception response status to 404

--- src/main/java/pl/jrostowski/filmwebscraper/exception/NotFoundException.java
+++ src/main/java/pl/jrostowski/filmwebscraper/exception/NotFoundException.java
@@ -1,5 +1,10 @@
 package pl.jrostowski.filmwebscraper.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ResponseStatus(code = NOT_FOUND)
 public abstract class NotFoundException extends RuntimeException {
 
     protected NotFoundException(String name, Long id) {
